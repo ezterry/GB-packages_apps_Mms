ab8279cb2b5bc7b9e45149c1163ea8d15d2842ba
Terry Hau <terryhau@gmail.com>
2011-04-26 17:08:11 -0700

    Made GMT offset optional for operators sent timestamp on received messages
    Change-Id: I6b334deaba6ab2c3b6825496368fa6204f44cfbf

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -61,6 +61,7 @@ public class MessagingPreferenceActivity extends PreferenceActivity {
     public static final String RETRIEVAL_DURING_ROAMING = "pref_key_mms_retrieval_during_roaming";
     public static final String AUTO_DELETE              = "pref_key_auto_delete";
     public static final String SENT_TIMESTAMP           = "pref_key_mms_use_sent_timestamp";
+    public static final String SENT_TIMESTAMP_GMT_CORRECTION = "pref_key_mms_use_sent_timestamp_gmt_correction";
 
     // Menu entries
     private static final int MENU_RESTORE_DEFAULTS    = 1;
